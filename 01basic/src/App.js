5af4604e785503797b7e3ad6d300b321db0f0904
Bikram Basnett <Bikrambasnett7@gmal.com>
2024-09-27 12:20:13 +0530

    Basic of React with setup guide

---
+++ 01basic/src/App.js
@@ -0,0 +1,15 @@
+import Basic from "./basic";
+function App() {
+  return (
+    // Here I have done some of the modification to run my custom h1 tag
+
+    <>
+  <h1>Hello World</h1>
+  
+  <Basic/>
+  <p>Basic Js file has been displayed</p>
+  </>
+  );
+}
+
+export default App;
---
+++ 01basic/src/basic.js
@@ -0,0 +1,13 @@
+
+// here also same rule applies that component first letter should be in a capital letter
+
+function Basic (){
+    return(
+
+        <>
+        <h1>This is a example of React </h1>
+        
+        </>
+    )
+}
+export default Basic;
