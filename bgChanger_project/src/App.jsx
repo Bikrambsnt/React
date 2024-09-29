118cd2877696ba233710fc4a2b1fe6bf65ea60e8
Bikram Basnett <Bikrambasnett7@gmal.com>
2024-09-29 19:09:43 +0530

    complete a basic bg changer project with react

--- bgChanger_project/src/App.jsx
+++ bgChanger_project/src/App.jsx
@@ -1,23 +1,26 @@
 import { useState } from 'react'
 
 function App() {
- let [color,setColor] = useState('balck');
+ let [color,setColor] = useState('purpleioererj');
   
 
   return (
     <>
     <div className='w-full h-screen flex  flex-wrap  justify-center items-center'
-    style={{backgroundColor:color}}> {/*here I dont have to use another curly braces because background color is already inside a double braces */}
+    style={{backgroundColor:color}}> {/*This is the syntax to use inline css in react ,here I dont have to use another curly braces because background color is already inside a double braces */}
        
        
        
         <div className='flex flex-wrap gap-4 text-white font-semibold'>
-          {/* <button onClick={()=> setColor('red') }  className='px-8 py-3 bg-red-600 outline-none rounded-xl'>Red</button> 
-          I can do it like this like this but the  */}
-          <button onClick={()=> setColor('red') }  className='px-8 py-3 bg-red-600 outline-none rounded-xl'>Red</button>
-          <button className='px-8 py-3 bg-blue-600 outline-none rounded-xl'>Blue</button>
-          <button className='px-8 py-3 bg-yellow-600 outline-none rounded-xl'>Yellow</button>
-          <button className='px-8 py-3 bg-orange-600 outline-none rounded-xl'>Orange</button>
+
+           {/* <button onClick={setColor('red') } className='px-8 py-3 bg-red-600 outline-none rounded-xl'>Red</button>  Here I cant assign value to the setCOlor function because onclick expext to return a call back function but here we are returning its refrence which onclick doesnt want so better we can do this*/}
+
+           <button onClick={()=> setColor('red') } className='px-8 py-3 bg-red-600 outline-none rounded-xl'>Red</button> 
+          {/*Here I am returning finction woth a function as onclick expect  */}
+
+          <button onClick={()=> setColor('blue')} className='px-8 py-3 bg-blue-600 outline-none rounded-xl'>Blue</button>
+          <button onClick={()=> setColor('yellow')} className='px-8 py-3 bg-yellow-600 outline-none rounded-xl'>Yellow</button>
+          <button onClick={()=> setColor('orange')} className='px-8 py-3 bg-orange-600 outline-none rounded-xl'>Orange</button>
         </div>
        
     </div>
