8d3afd0858d0ae15c925e5abeca170a6c4b0b65f
Bikram Basnett <Bikrambasnett7@gmal.com>
2024-10-04 12:45:16 +0530

    Complete a basic todo Project with Ccontext API

--- contextApi_with_TODO_list/src/App.jsx
+++ contextApi_with_TODO_list/src/App.jsx
@@ -76,7 +76,7 @@ useEffect(()=>{
   return (
     <TodoProvider value={{todos,addTodo,updateTodo,deleteTodo,toggleComplete}}>
     <div className="bg-[#172842] min-h-screen  py-8">
-                <div className="w-full max-w-2xl  mx-auto shadow-md rounded-lg px-4 py-3 text-white">
+                <div className="w-full max-w-2xl bg-[#c5c5c5] mx-auto shadow-md rounded-lg px-4 py-3 text-white">
                     <h1 className="text-2xl font-bold text-center mb-8 mt-2">Add Your Todos</h1>
                     <div className="mb-4">
                         {/* Todo form goes here */} 
